b6cff7e5b5828de1d744899a185e5a5233b79c75
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-20 02:13:56 -0600

    nav index.js added

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -2,7 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
-    return ()
+    return (
+        <header>
+            <Link to='/'>
+                <h1>mattresses by leo</h1>
+            </Link>
+            <Link to ='/products'>
+            </Link>
+            <Link to ='/search'>
+            </Link>
+            <Link to ='/Cart'>
+            </Link>
+        </header>
+    )
+    
 }
 
 export default Nav
