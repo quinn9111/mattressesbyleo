32fd92b8715cb5e5acfdb893953a5dbb3462b4f5
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-19 20:45:06 -0600

    components added: nav, products dir, building home

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,12 +1,18 @@
 import { css } from '@emotion/react'
 import { Link } from 'react-router-dom'
+import Categories from '../categories'
+import ProductList from '../products/productList'
 
 const Home = () => {
     return (
         <>
             <div className='container home-page'>
-                
+            <Categories />
+            <ProductList />
+            /*Cart
             </div>
         </>
     )
 }
+
+export default Home
--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -3,10 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
     return (
-        <div className='app'>
-            <nav>
-
-            </nav>
+        <div className='site'>
+            <Nav />
             <div className='page'>
 
             <Outlet />
