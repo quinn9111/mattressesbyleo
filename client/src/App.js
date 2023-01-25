928c626210f9b6947def2a3d87fec6511e05ab99
Jessica Medina <lic.jessica.medina@gmail.com>
2023-01-24 22:04:47 -0600

    fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,34 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 import Home from './components/home'
 import ProductList from './components/products/productList'
 import Login from './components/login'
-import Cart from './components/cart'
+//import Cart from './components/cart'
 import React, { useState } from 'react'
-import ProductList from './components/products/productList'
+
 
 
 function App() {
-  const [width, setWindowWidth] = useState(0)
+/*   const [width, setWindowWidth] = useState(0)
 
   const updateDimensions = () => {
     width = window.innerWidth
     setWindowWidth(width)
 
     updateDimensions()
-  }
+  } */
 
   return (
     <>
+    <Router>
       <Routes>
-          <Route path='/' element={<Layout />} />
-            <Route index element={<Home />} />
-            <Route path='products' element={<ProductList />} />
-            <Route path='login' element={<Login />} />
-            <Route path='cart' element={<Cart />} />
+          <Route path='/' element={<Login />} />
+            <Route path='/home' element= {<Home />} />
+            <Route path='/products' element={<ProductList />} /> 
+              {/* <Route path='/cart' element={<Cart />} />  */}
       </Routes>
+      </Router>
     </>
   )
 }
--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,6 +1,5 @@
-import Categories from '../categories'
 import ProductList from '../products/productList'
-import Search from './search'
+//import Search from './search'
 import Nav from '../nav'
 import { Link } from 'react-router-dom'
 
@@ -8,13 +7,10 @@ const Home = () => {
     return (
         <>
         <div className='container home-page'>
-            <div className='container categories'>
-            <Categories />
-            </div>
             //cart
         </div>
         </>
-    )å
+    )
 }
 
 export default Home
